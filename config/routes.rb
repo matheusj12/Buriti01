6fc41c0ce29966ae8044f8113c99c491e2c88aef
matheusj12 <matheusjulio12@hotmail.com>
2024-09-02 17:58:53 -0300

    atu_artigos02092024

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,17 @@
 Rails.application.routes.draw do
-  resources :gestao_beneficios
-  resources :automacao_tarefas
-  resources :projetos
-  resources :relatorios
-  resources :cadastro_ativos
-  resources :ponto_eletronicos
-  resources :processo_admissaos
-  resources :folha_pagamentos
-  resources :pedido_vendas
-  resources :consulta_estoques
-  resources :fornecedors
-  resources :pedido_compras
-  resources :ordem_pagamentos
-  resources :gerenciamento_contas_bancaria
-  resources :titulos_baixados
-  resources :titulos_em_abertos
-  resources :tesouraria
-  resources :fluxo_caixas
-  resources :conta_recebers
-  resources :conta_pagars
-  resources :compras
-  resources :departamentos
+  # Define a rota root para o index da home
   root 'home#index'
 
+  # Configura as rotas do Devise para autenticação de usuários
   devise_for :users
 
-  resources :products
-  resources :clients
-  resources :vendas
-  resources :servicos
-  resources :funcionarios
-  resources :automacao_tarefas
+  # Define todas as rotas RESTful para artigos (index, show, new, create, edit, update, destroy)
+  resources :articles
 
+  # Rota explícita para o index da home, com alias 'home_index'
+  get 'home/index', as: 'home_index'
 
-  get 'home/index', as: 'home_index'  # Adicione esta linha
-
-
+  # Outras rotas e configurações adicionais...
 end
   # ... outras rotas e configurações adicionais ...
   # rails server -b 10.0.60.189
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_26_132934) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_02_201233) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.text "content"
+    t.datetime "published_at"
+    t.string "author_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "body"
+    t.string "author"
+    t.string "tags"
+  end
+
   create_table "automacao_tarefas", force: :cascade do |t|
     t.string "nome"
     t.text "descricao"
