50098fd9cef4365084423a5dab4540908fba12dd
matheusj12 <matheusjulio12@hotmail.com>
2024-11-05 13:49:40 -0300

    commiterrovendas

--- db/migrate/20231227122616_add_data_to_vendas.rb
+++
@@ -1,5 +0,0 @@
-class AddDataToVendas < ActiveRecord::Migration[7.0]
-  def change
-    add_column :vendas, :data, :date
-  end
-end
--- db/migrate/20240822155052_add_client_id_to_vendas.rb
+++
@@ -1,5 +0,0 @@
-class AddClientIdToVendas < ActiveRecord::Migration[7.2]
-  def change
-    add_column :vendas, :client_id, :integer
-  end
-end
